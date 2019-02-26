68169b16358147f4e952d45202abdc3dd5994dac
Maiquel Knechtel <maiquelknechtel@gmail.com>
2019-02-25 22:33:11 -0300

    Initial Commit

---
+++ appOpengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  appOpengl
+//
+//  Created by Maiquel Knechtel on 25/02/19.
+//  Copyright © 2019 Maiquel Knechtel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
